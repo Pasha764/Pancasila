c3b544fd5abcd81538fb5bec8dfee5497a3dfa42
Riss-ST <149866897+Riss-ST@users.noreply.github.com>
2025-12-07 20:35:31 +0700

    Update main mechanic spawn kebutuhan
    Update main mechanic random spawn req kebutuhan npc, npc movement, setengah jalan drag and drop (baru set ui di tiap kebutuhan)

---
+++ Assets/Scripts/NPCDrop.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NPCDrop : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        var item = eventData.pointerDrag.GetComponent<ItemDrag>();
+        if (item != null)
+        {
+            Debug.Log("NPC menerima item: " + item.itemName);
+        }
+    }
+}
